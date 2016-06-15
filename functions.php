d4412896c8d30943da8d3b6a5871086b27005a71
Jaiden Mispy <jaiden@mispy.me>
2016-06-15 21:26:56 +1000

    Footer is now generally decent

--- functions.php
+++ functions.php
@@ -348,35 +348,51 @@ remove_action( 'genesis_footer', 'genesis_do_footer' );
 add_action( 'genesis_footer', 'sp_custom_footer' );
 function sp_custom_footer() {
 	?>
-	<div>
+	<div class="clearfix">
 		<div class="column">
-			<h4>Our World In Data</h4>
-			<p>OWID is an online publication developed at the <a href="http://www.oxfordmartin.ox.ac.uk/research/programmes/world-data">University of Oxford</a> which illustrates how living conditions are changing around the world. More information can be found on the <a href="/about">about page</a>.</p>
+			<h4><a href="/">Our World In Data</a></h4>
+			<p>OWID is an online publication developed at the <a href="http://www.oxfordmartin.ox.ac.uk/research/programmes/world-data">University of Oxford</a> which shows how living conditions are changing around the world. Our work is licensed under <a href="http://creativecommons.org/licenses/by-sa/4.0/deed.en_US">CC BY-SA</a> and freely reusable by journalists or educators.</p>
 		</div>
 		<div class="column">
 			<h6>Sign up to stay informed</h6>
-			<form id="footer-email-signup">
-				<input type="text" value="Email"><input class="button" type="submit" value="Subscribe">
-			</form>
-			<h6>Follow OWID</h6>
+
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="//ourworldindata.us8.list-manage.com/subscribe/post?u=18058af086319ba6afad752ec&amp;id=2e166c1fc1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">				
+						<div class="mc-field-group">
+							<input type="email" value="" placeholder="Email" name="EMAIL" class="required email" id="mce-EMAIL"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+						</div>
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_18058af086319ba6afad752ec_2e166c1fc1" tabindex="-1" value=""></div>
+				    </div>
+				</form>
+				</div>
+			<script type='text/javascript' src='/wp-content/themes/parallax-pro/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			<!--End mc_embed_signup-->
+
+			<h6>Follow us</h6>
 	    	<div class="social">
 	    		<a href="https://twitter.com/MaxCRoser"><i class="fa fa-twitter"></i></a>
 	    		<a href="https://www.facebook.com/OurWorldinData"><i class="fa fa-facebook"></i></a>
 	    	</div>			
 		</div>
-		<div class="column">
+		<div class="column links">
 			<h6>Links</h6>
 			<a href="/data">Entries</a>
 			<a href="/blog">Blog</a>
 			<a href="/support">Donate</a>
+			<a href="/about">About</a>
 			<a href="/wp-admin">Login</a>
 		</div>
 	</div>
 	<div class="supporters">
-		<h6>Supported By</h6>
 		<!--<img src="/wp-content/themes/parallax-pro/images/oxford-martin-school.png">-->
-		<img src="/wp-content/themes/parallax-pro/images/inet-oxford.png">
-		<img src="/wp-content/themes/parallax-pro/images/nuffield-foundation.png">		
+		<a href="http://www.inet.ox.ac.uk/"><img src="/wp-content/themes/parallax-pro/images/inet-oxford.png"></a>
+		<a href="http://www.nuffieldfoundation.org/"><img src="/wp-content/themes/parallax-pro/images/nuffield-foundation.png"></a>
 	</div>
 	<?php
 }
