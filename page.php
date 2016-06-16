d437f42a1d21f1d49600746a2d81f8cd0129b2c6
Jaiden Mispy <jaiden@mispy.me>
2016-06-16 21:36:09 +1000

    Starring works in header menu again

--- page.php
+++ page.php
@@ -14,24 +14,25 @@
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
-	<?php
-	/* MAX: Add Author Byline in Pages - so that it is citable (Exclude 'About-Page' and 'Data-Page' from this) */
-	if ( !is_page('about') &&  !is_page('data') &&  !is_page('owid-grapher') &&  !is_page('web-developer') && !is_page('support') && !is_page('job-offer-quantitative-social-scientist-for-ourworldindata-org-at-the-university-of-oxford') && !is_page ('job-offer-web-developer-for-ourworldindata-org-at-the-university-of-oxford') && is_page() ) {
-	    $authors = coauthors(null, null, null, null, false);
-
-	    if (strpos($authors, "Max Roser") === false) {
-	      $authors = $authors . " and Max Roser";
-	    }
-
-	    $posttitle = 'Please cite the original source – including the original data source – and this entry on Our World In Data.<br>This entry can be cited as '.$authors.' ('.get_the_modified_date('Y').') – &lsquo;'.get_the_title($ID).'&rsquo;. <em>Published online at OurWorldInData.org.</em> Retrieved from: '.get_permalink().' [Online Resource]';
-
-	    echo "<div class=\"post-byline\">$posttitle</div>";
-	}
-	?>
 
 	<?php while (have_posts()) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<header class="entry-header">
+				<?php
+				/* MAX: Add Author Byline in Pages - so that it is citable (Exclude 'About-Page' and 'Data-Page' from this) */
+				if ( !is_page('about') &&  !is_page('data') &&  !is_page('owid-grapher') &&  !is_page('web-developer') && !is_page('support') && !is_page('job-offer-quantitative-social-scientist-for-ourworldindata-org-at-the-university-of-oxford') && !is_page ('job-offer-web-developer-for-ourworldindata-org-at-the-university-of-oxford') && is_page() ) {
+				    $authors = coauthors(null, null, null, null, false);
+
+				    if (strpos($authors, "Max Roser") === false) {
+				      $authors = $authors . " and Max Roser";
+				    }
+
+				    $posttitle = 'Please cite the original source – including the original data source – and this entry on Our World In Data.<br>This entry can be cited as '.$authors.' ('.get_the_modified_date('Y').') – &lsquo;'.get_the_title($ID).'&rsquo;. <em>Published online at OurWorldInData.org.</em> Retrieved from: '.get_permalink().' [Online Resource]';
+
+				    echo "<div class=\"post-byline\">$posttitle</div>";
+				}
+				?>
+
 				<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 			</header><!-- .entry-header -->
 
