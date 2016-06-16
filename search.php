d18107780ed0dc86f8276de66201d2b39b9904b6
Jaiden Mispy <jaiden@mispy.me>
2016-06-16 23:54:58 +1000

    Tweaks to entry styling

--- search.php
+++ search.php
@@ -1,54 +1,45 @@
 <?php
-	/*
-		Template Name: Search results
-		Author: Zdenek Hynek, zdenek.hynek@gmail.com
-	*/
+/**
+ * The template for displaying search results pages
+ */
 
-	/**
-	 * modified version of genesis/search.php
-	 */
+get_header(); ?>
 
-	add_action('genesis_before_loop', 'genesis_do_search_title');
-	function genesis_do_search_title() {
-		$title = sprintf( '<div class="search-results-header"><h1>%s <em>%s</em></h1></div>', apply_filters( 'genesis_search_title_text', __( 'Search Results for:', 'genesis' ) ), get_search_query() );
-		echo apply_filters('genesis_search_title_output', $title) . "\n";
-	}
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-	remove_action('genesis_loop', 'genesis_do_loop');
-	add_action('genesis_loop', 'custom_loop');
-	function custom_loop() { 
-		global $search_results_per_page;
+		<?php if (have_posts()) : ?>
+			<header class="page-header">
+				<h1 class="page-title"><?php printf('Search Results for: %s', '<span>' . esc_html(get_search_query()) . '</span>'); ?></h1>
+			</header><!-- .page-header -->
 
-		?>
-		<div class="search-results">
-			<?php 
-			if (have_posts()) :
-				while (have_posts()) : the_post(); 
-					//exclude list of topics (4723), and data sitemap (621)
-					$pages_to_exclude = array( "4723", "621" );
-					if( in_array( get_the_id(), $pages_to_exclude ) ) {
-						continue;	
-					}
-				?>
-				<div class="post type-post search-post hentry">
-					<h2 class="search-entry-title entry-title"><a href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-					<div class="entry-summary">
-						<div class="prod-list-content">
-							<a class="more-link" href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>" >
-								<?php the_excerpt(); ?>
-							</a>
-						</div><!-- .prod-list-content -->
-					</div><!-- .entry-summary -->
-				</div><!-- .post -->
-				 
-			<?php 
-				endwhile; 
-			endif; 
-			?>	
-		</div><!-- .search-results -->
+			<?php while (have_posts()) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	 				  	<div class='entry-meta'><time>Updated <?php the_modified_date("F d, Y"); ?></time> by <?php the_author(); ?></div>
+					</header><!-- .entry-header -->
 
-		<?php echo genesis_posts_nav(); ?>
-	<?php
-	}
-genesis();
-?>
+					<div class="entry-content">
+						<?php the_excerpt(); ?>
+					</div><!-- .entry-content -->
+
+					<footer class="entry-footer">
+					</footer><!-- .entry-footer -->
+				</article><!-- #post-## -->
+			<?php endwhile ?>;
+
+			<?php the_posts_pagination([
+			'prev_text'          => '« Prev',        
+			'next_text'          => 'Next »',             
+			'before_page_number' => '',
+			]) ?>  
+
+		<?php else : ?>
+			<h1 class="page-title no-results">No results found for <?php echo esc_html(get_search_query()) ?></h1>
+		<?php endif; ?>
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php get_footer(); ?>
