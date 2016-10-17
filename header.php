e48021e54c50e4f8cef20b863d2e0c40c6023086
Jaiden Mispy <jaiden@mispy.me>
2016-10-18 04:47:16 +1100

    Use proper 404 page with prefilled search form

--- header.php
+++ header.php
@@ -23,7 +23,6 @@
 	
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 	<?php wp_head(); ?>
 
