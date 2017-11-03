1ae0f8f7e787550257f0544f97924f0ee6238b13
Jaiden Mispy <jaiden@mispy.me>
2017-11-03 18:20:25 +0800

    Use proper thumbnail images and fixed heights

--- home.php
+++ home.php
@@ -11,7 +11,7 @@ get_header(); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<li class="post">
 			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail(); ?>
+				<?php the_post_thumbnail('medium'); ?>
 				<h3><?php the_title(); ?></h3>
 				<div class='entry-meta'><time><?php the_date("F d, Y"); ?></time> by <?php
 						$coauthors = coauthors(null, null, null, null, false);
