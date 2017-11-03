63ca0521f546613be3c9312050937f652b4ae24b
Jaiden Mispy <jaiden@mispy.me>
2017-11-03 17:58:51 +0800

    Redesign blog index page

--- home.php
+++ home.php
@@ -5,29 +5,22 @@
 
 get_header(); ?>
 
-<header class="blog-header">
-	<h1>Blog</h1>
-</header>
-
 <div class="site-content">
+	<h2>Latest Posts</h2>
+	<ul class="posts">
 	<?php while ( have_posts() ) : the_post(); ?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="article-header">
-				<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-			  	<div class='entry-meta'><time><?php the_date("F d, Y"); ?></time> by <?php
-					$coauthors = coauthors(null, null, null, null, false);
-					echo $coauthors;
-			  	?></div>
-			</header><!-- .article-header -->
-
-			<div class="article-content">
-				<?php the_content(); ?>
-			</div><!-- .article-content -->
-
-			<footer class="article-footer">
-			</footer><!-- .article-footer -->
-		</article><!-- #post-## -->
-	<?php endwhile; ?>			
+		<li class="post">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail(); ?>
+				<h3><?php the_title(); ?></h3>
+				<div class='entry-meta'><time><?php the_date("F d, Y"); ?></time> by <?php
+						$coauthors = coauthors(null, null, null, null, false);
+						echo $coauthors;
+					  ?></div>
+			</a>
+		</li>
+	<?php endwhile; ?>
+	</ul>
 
 	<?php the_posts_pagination([
 		'prev_text'          => '« Prev',        
