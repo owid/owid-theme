7b2896db2ccc9607143a2e31ce8a5378613d4006
Jaiden Mispy <jaiden@mispy.me>
2017-12-23 20:29:46 +0800

    Remove unnecessary stuff

--- front-page.php
+++ front-page.php
@@ -1,4 +1,11 @@
 <?php 
+	// Redirect from admin site for live urls
+	if (!is_preview() && strpos(get_the_permalink(), "https://owid.cloud") !== false) {
+		$url = str_replace("https://owid.cloud", "https://ourworldindata.org", get_the_permalink());
+		wp_redirect($url, 302);
+		exit;
+	}
+
 	$themeDir = dirname(__FILE__);
 	$cmd = "cd $themeDir && node dist/src/renderPage.js front";
 	error_log($cmd);
--- page.php
+++ page.php
@@ -1,5 +1,12 @@
 <?php 
 	the_post();
+
+	if (get_post_status() == 'publish') {
+		$url = str_replace("http://l:8080", "https://ourworldindata.org", get_the_permalink());
+		wp_redirect($url, 302);
+		exit;
+	}
+
 	$ID = escapeshellarg(get_the_ID());
 	$themeDir = dirname(__FILE__);
 	$cmd = "cd $themeDir && node dist/src/renderPage.js $ID";
