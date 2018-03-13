cdbb8773c6d39a53b7eae1482003b7258d07d183
Jaiden Mispy <jaiden@mispy.me>
2018-03-13 22:48:05 +0800

    Organize redirects

--- src/BakeWordpress.tsx
+++ src/BakeWordpress.tsx
@@ -39,20 +39,32 @@ export default class WordpressBaker {
             // Let's Encrypt certbot verification 
             "/.well-known/* https://owid.cloud/.well-known/:splat 200",
             
+            // RSS feed
             "/feed /atom.xml 302",
-            "/entries /#entries 302",
-            "/chart-builder/* /grapher/:splat 301",
-            "/grapher/public/* /grapher/:splat 301",
-            "/grapher/view/* /grapher/:splat 301",
+
+            // Backwards compatibility-- admin urls
             "/wp-admin/* https://owid.cloud/wp-admin/:splat 302",
             "/wp-login.php https://owid.cloud/wp-login.php 302",
             "/grapher/admin/* https://owid.cloud/grapher/admin/:splat 302",
+
+            // Backwards compatibility-- old Max stuff that isn't static-friendly
             "/roser/* https://www.maxroser.com/roser/:splat 302",
             "/wp-content/uploads/nvd3/* https://www.maxroser.com/owidUploads/nvd3/:splat 302",
             "/wp-content/uploads/datamaps/* https://www.maxroser.com/owidUploads/datamaps/:splat 302",
-            "/grapher/* https://owid-grapher.netlify.com/grapher/:splat 200",
             "/slides/Max_PPT_presentations/* https://www.maxroser.com/slides/Max_PPT_presentations/:splat 302",
-            "/slides/Max_Interactive_Presentations/* https://www.maxroser.com/slides/Max_Interactive_Presentations/:splat 302"
+            "/slides/Max_Interactive_Presentations/* https://www.maxroser.com/slides/Max_Interactive_Presentations/:splat 302",
+
+            // Backwards compatibility-- public urls
+            "/entries /#entries 302",
+            "/data/food-agriculture/land-use-in-agriculture /yields-and-land-use-in-agriculture 301",
+
+            // Backwards compatibility-- grapher url style
+            "/chart-builder/* /grapher/:splat 301",
+            "/grapher/public/* /grapher/:splat 301",
+            "/grapher/view/* /grapher/:splat 301",
+
+            // Main grapher chart urls are proxied through to separate repo
+            "/grapher/* https://owid-grapher.netlify.com/grapher/:splat 200",
         ]
     
         const rows = await wpdb.query(`SELECT url, action_data, action_code FROM wp_redirection_items`)
