4faace9600c160fd8e5fee01465d51ad55962a66
Jaiden Mispy <jaiden@mispy.me>
2018-08-14 14:53:24 +0800

    Remove markdown formatting code for now

--- src/formatting.tsx
+++ src/formatting.tsx
@@ -10,17 +10,7 @@ import Tablepress from './views/Tablepress'
 import {GrapherExports} from './grapherUtil'
 import * as path from 'path'
 
-//const compiler = require('markdown-to-jsx').compiler
-const MarkdownIt = require('markdown-it')
 const mjAPI = require("mathjax-node");
-
-
-
-const md = new MarkdownIt({ html: true, linkify: true })
-export function parseMarkdown(content: string): string {
-    //return compiler(content).props.children||[]
-    return md.render(content)
-}
 
 export interface FormattedPost {
     id: number
@@ -58,7 +48,6 @@ mjAPI.config({
 });
 mjAPI.start();
 
-
 function extractLatex(html: string): [string, string[]] {
     const latexBlocks: string[] = []
     html = html.replace(/\[latex\]([\s\S]*?)\[\/latex\]/gm, (_, latex) => {
@@ -89,7 +78,7 @@ async function formatLatex(html: string, latexBlocks?: string[]): Promise<string
     })
 }
 
-export async function formatPostLegacy(post: FullPost, html: string, grapherExports?: GrapherExports) {
+export async function formatWordpressPost(post: FullPost, html: string, grapherExports?: GrapherExports) {
     // Strip comments
     html = html.replace(/<!--[^>]+-->/g, "")
     
@@ -182,6 +171,8 @@ export async function formatPostLegacy(post: FullPost, html: string, grapherExpo
     // Image processing
     const uploadDex = await getUploadedImages()
     for (const el of $("img").toArray()) {
+        const $el = $(el)
+
         // Open full-size image in new tab
         if (el.parent.tagName === "a") {
             el.parent.attribs['target'] = '_blank'
@@ -193,165 +184,16 @@ export async function formatPostLegacy(post: FullPost, html: string, grapherExpo
         if (upload && upload.variants.length) {
             el.attribs['srcset'] = upload.variants.map(v => `${v.url} ${v.width}w`).join(", ")
             el.attribs['sizes'] = "(min-width: 800px) 50vw, 100vw"
-        }
-    }
-
-    // Table of contents and deep links
-    const hasToc = post.type === 'page' && post.slug !== 'about'
-    let openHeadingIndex = 0
-    let openSubheadingIndex = 0
-    const tocHeadings: { text: string, slug: string, isSubheading: boolean }[] = []
-    $("h1, h2, h3, h4").each((_, el) => {
-        const $heading = $(el);
-        const headingText = $heading.text()
-        // We need both the text and the html because may contain footnote
-        let headingHtml = $heading.html() as string
-        const slug = urlSlug(headingText)
-
-        // Table of contents
-        if (hasToc) {
-            if ($heading.is("#footnotes") && footnotes.length > 0) {
-                tocHeadings.push({ text: headingText, slug: "footnotes", isSubheading: false })
-            } else if (!$heading.is('h1') && !$heading.is('h4')) {
-                // Inject numbering into the text as well
-                if ($heading.is('h2')) {
-                    openHeadingIndex += 1;
-                    openSubheadingIndex = 0;
-                } else if ($heading.is('h3')) {
-                    openSubheadingIndex += 1;
-                }
-    
-                if (openHeadingIndex > 0) {
-                    if ($heading.is('h2')) {
-                        headingHtml = romanize(openHeadingIndex) + '. ' + headingHtml;
-                        $heading.html(headingHtml)
-                        tocHeadings.push({ text: $heading.text(), slug: slug, isSubheading: false })
-                    } else {
-                        headingHtml = romanize(openHeadingIndex) + '.' + openSubheadingIndex + ' ' + headingHtml;
-                        $heading.html(headingHtml)
-                        tocHeadings.push({ text: $heading.text(), slug: slug, isSubheading: true })
-                    }					
-                }
-            }    
-        }
-
-        // Deep link
-        $heading.attr('id', slug).prepend(`<a class="deep-link" href="#${slug}"></a>`)
-    })
-
-    return {
-        id: post.id,
-        type: post.type,
-        slug: post.slug,
-        title: post.title,
-        date: post.date,
-        modifiedDate: post.modifiedDate,
-        authors: post.authors,
-        html: $("body").html() as string,
-        footnotes: footnotes,
-        excerpt: post.excerpt || $($("p")[0]).text(),
-        imageUrl: post.imageUrl,
-        tocHeadings: tocHeadings
-    }
-}
-
-export async function formatPostMarkdown(post: FullPost, html: string, grapherExports?: GrapherExports) {    
-    // Remove starting tag
-    html = html.replace(/^<!--markdown-->/, "")
-
-    // Footnotes
-    const footnotes: string[] = []
-    html = html.replace(/\[ref\]([\s\S]*?)\[\/ref\]/gm, (_, footnote) => {
-        footnotes.push(parseMarkdown(footnote))
-        const i = footnotes.length
-        return `<a id="ref-${i}" class="ref" href="#note-${i}"><sup>${i}</sup></a>`
-    })
-
-    html = await formatLatex(html)
-
-    // Insert [table id=foo] tablepress tables
-    const tables = await getTables()
-    html = html.replace(/\[table\s+id=(\d+)\s*\/\]/g, (match, tableId) => {
-        const table = tables.get(tableId)
-        if (table)
-            return ReactDOMServer.renderToStaticMarkup(<Tablepress data={table.data}/>)
-        else
-            return "UNKNOWN TABLE"
-    })
-
-    html = parseMarkdown(html)
-
-    // These old things don't work with static generation, link them through to maxroser.com
-    html = html.replace(new RegExp("/wp-content/uploads/nvd3", 'g'), "https://www.maxroser.com/owidUploads/nvd3")
-            .replace(new RegExp("/wp-content/uploads/datamaps", 'g'), "https://www.maxroser.com/owidUploads/datamaps")
-
-    const $ = cheerio.load(html)
-
-    // Wrap content demarcated by headings into section blocks
-    const sectionStarts = [$("body").children().get(0)].concat($("h2").toArray())
-    for (const start of sectionStarts) {
-        const $start = $(start)
-        const $contents = $start.nextUntil("h2")
-        const $wrapNode = $("<section></section>");
-
-        $contents.remove();
-        $wrapNode.append($start.clone())
-        $wrapNode.append($contents)
-        $start.replaceWith($wrapNode)
-    }
-
-    // Replace grapher iframes with static previews
-    if (grapherExports) {
-        const grapherIframes = $("iframe").toArray().filter(el => (el.attribs['src']||'').match(/\/grapher\//))
-        for (const el of grapherIframes) {
-            const src = el.attribs['src']
-            const chart = grapherExports.get(src)
-            if (chart) {
-                const output = `<figure data-grapher-src="${src}" class="grapherPreview"><a href="${src}" target="_blank"><div><img src="${chart.svgUrl}"/></div></a></div>`
-                $(el).replaceWith(output)
-            }
-        }
-    }
-
-    // Any remaining iframes: ensure https embeds
-    if (HTTPS_ONLY) {
-        for (const iframe of $("iframe").toArray()) {
-            iframe.attribs['src'] = iframe.attribs['src'].replace("http://", "https://")
-        }
-    }
-
-    // Remove any empty elements
-    for (const p of $("p").toArray()) {
-        const $p = $(p)
-        if ($p.contents().length === 0)
-            $p.remove()
-    }
-
-    // Image processing
-    const uploadDex = await getUploadedImages()
-    for (const el of $("img").toArray()) {
-        const $el = $(el)
-
-        // Set srcset to load image responsively
-        const src = el.attribs['src']||""
-        const upload = uploadDex.get(path.basename(src))
-        if (upload && upload.variants.length) {
-            el.attribs['srcset'] = upload.variants.map(v => `${v.url} ${v.width}w`).join(", ")
-            el.attribs['sizes'] = "(min-width: 800px) 50vw, 100vw"
 
             // Link through to full size image
-
-            if (el.parent.tagName === "a") {
-                el.parent.attribs['target'] = '_blank'
-            } else {
+            if (el.parent.tagName !== "a") {
                 const $a = $(`<a href="${upload.originalUrl}" target="_blank"></a>`)
                 $el.replaceWith($a)
                 $a.append($el)
             }
         }
-
     }
-
+    
     // Table of contents and deep links
     const hasToc = post.type === 'page' && post.slug !== 'about'
     let openHeadingIndex = 0
@@ -418,6 +260,8 @@ export async function formatPost(post: FullPost, grapherExports?: GrapherExports
     html = html.replace(new RegExp(WORDPRESS_URL, 'g'), BAKED_URL)
         .replace(new RegExp("https?://ourworldindata.org", 'g'), BAKED_URL)
 
+    // If <!--raw--> appears at the top of a post, it signals that the author
+    // wants to bypass the formatting and just write plain HTML
     const isRaw = html.match(/<!--raw-->/)
 
     if (isRaw) {
@@ -435,10 +279,8 @@ export async function formatPost(post: FullPost, grapherExports?: GrapherExports
             imageUrl: post.imageUrl,
             tocHeadings: []
         }
-    } else if (html.match(/^<!--markdown-->/)) {
-        return formatPostMarkdown(post, html, grapherExports)
     } else {
-        return formatPostLegacy(post, html, grapherExports)
+        return formatWordpressPost(post, html, grapherExports)
     }
 }
 
