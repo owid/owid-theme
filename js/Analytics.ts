c2f7a13a68587346c222feb3b3fccc1257cc23ec
Jaiden Mispy <jaiden@mispy.me>
2019-01-01 18:44:10 +0800

    Ensure analytics event goes through before submitting form

--- js/Analytics.ts
+++ js/Analytics.ts
@@ -1,11 +1,16 @@
 declare var window: any
 
 export class Analytics {
-    static logEvent(name: string, props?: any) {
-        if (!window.amplitude)
-            console.log(name, props)
-        else {
-            window.amplitude.getInstance().logEvent(name, props)
-        }
+    static logEvent(name: string, props?: any): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!window.amplitude) {
+                console.log(name, props)
+                resolve()
+            } else {
+                window.amplitude.getInstance().logEvent(name, props, () => {
+                    resolve()
+                })
+            }
+        })
     }
 }
--- js/owid.entry.ts
+++ js/owid.entry.ts
@@ -2,9 +2,13 @@ import '../css/style.scss'
 import './oldScripts.js'
 import {Analytics} from './Analytics'
 
-const search = document.querySelector("form#search-nav")
+const search = document.querySelector("form#search-nav") as HTMLFormElement
 
 if (search) {
     const input = search.querySelector("input[type=search]") as HTMLInputElement
-    search.addEventListener('submit', () => Analytics.logEvent("OWID_SITE_SEARCH", { query: input.value }))
+    let lastQuery = ""
+    search.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        Analytics.logEvent("OWID_SITE_SEARCH", { query: input.value }).then(() => search.submit()).catch(() => search.submit())
+    })
 }
