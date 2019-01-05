2a88e0fa1223bd9ebc97959d70a2f2eaebb4909f
Jaiden Mispy <jaiden@mispy.me>
2019-01-05 09:35:35 -0800

    Update to webpack 4, dependencies update

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,105 @@
 const path = require('path')
 const webpack = require('webpack')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 
-const isProduction = process.argv.indexOf('-p') !== -1
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
 
-module.exports = {
-    context: __dirname,
-    entry: {
-        owid: "./js/owid.entry.ts",
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "js/[name].js"
-    },
-  	resolve: {
-        extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
-        alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat',
+    return {
+        context: __dirname,
+        mode: isProduction ? "production" : "development",
+        entry: {
+            owid: "./js/owid.entry.ts",
         },
-        modules: [
-  	        path.join(__dirname, "js/libs"),
-            path.join(__dirname, "css/libs"),
-            path.join(__dirname, "node_modules"),
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true
-                }
+        output: {
+            path: path.join(__dirname, "dist"),
+            filename: "js/[name].js"
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+            alias: {
+                'react': 'preact-compat',
+                'react-dom': 'preact-compat',
             },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader?modules&importLoaders=1&localIdentName=[local]'],
-                }),
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader'] })
-            },
-            {
-                test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
-                loader: 'url-loader?limit=10000'
-            }
-        ],
-    },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: (isProduction ? false : "cheap-module-eval-source-map"),
-
-    plugins: (isProduction ? [
-        // This plugin extracts css files required in the entry points
-        // into a separate CSS bundle for download
-        new ExtractTextPlugin('[name].css'),
-
-        // CSS optimization
-        /*new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.bundle.*\.css$/,
-            cssProcessorOptions: { discardComments: { removeAll: true } }
-        }),*/
-
-        // JS optimization
-        new ParallelUglifyPlugin({
-            cachePath: path.join(__dirname, 'tmp'),
-            uglifyJS: {
-                compress: {
-                  warnings: false,
-                  conditionals: true,
-                  unused: false,
-                  comparisons: true,
-                  sequences: true,
-                  dead_code: true,
-                  evaluate: true,
-                  if_return: true,
-                  join_vars: true
+            modules: [
+                path.join(__dirname, "js/libs"),
+                path.join(__dirname, "css/libs"),
+                path.join(__dirname, "node_modules"),
+            ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true
+                    }
                 },
-            }
-        }),
-    ] : [
-        new ExtractTextPlugin('[name].css')
-    ]),
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader?modules&importLoaders=1&localIdentName=[local]'],
+                    }),
+                },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader'] })
+                },
+                {
+                    test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
+                    loader: 'url-loader?limit=10000'
+                }
+            ],
+        },
 
-    devServer: {
-        host: 'localhost',
-        port: 8095,
-        contentBase: 'public',
-        disableHostCheck: true,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: (isProduction ? false : "cheap-module-eval-source-map"),
+
+        plugins: (isProduction ? [
+            // This plugin extracts css files required in the entry points
+            // into a separate CSS bundle for download
+            new ExtractTextPlugin('[name].css'),
+
+            // CSS optimization
+            /*new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.bundle.*\.css$/,
+                cssProcessorOptions: { discardComments: { removeAll: true } }
+            }),*/
+
+            // JS optimization
+            new ParallelUglifyPlugin({
+                cachePath: path.join(__dirname, 'tmp'),
+                uglifyJS: {
+                    compress: {
+                    warnings: false,
+                    conditionals: true,
+                    unused: false,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true
+                    },
+                }
+            }),
+        ] : [
+            new ExtractTextPlugin('[name].css')
+        ]),
+
+        devServer: {
+            host: 'localhost',
+            port: 8095,
+            contentBase: 'public',
+            disableHostCheck: true,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            }
         }
-    },
+    }
 }
