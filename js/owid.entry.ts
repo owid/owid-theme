dbd7bf9a88e62c78f1748c2cd822bb0419d479ff
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-01-07 12:16:43 -0800

    Implement generic click tracking with amplitude
    Can set a `data-track-click` attribute to track clicks on an anchor.

--- js/owid.entry.ts
+++ js/owid.entry.ts
@@ -13,3 +13,30 @@ if (search) {
         Analytics.logEvent("OWID_SITE_SEARCH", { query: input.value }).then(() => search.submit()).catch(() => search.submit())
     })
 }
+
+function getParent(el: HTMLElement, condition: Function): HTMLElement | null {
+    let current: HTMLElement | null = el
+    while (current) {
+        if (condition(current)) return current
+        current = current.parentElement
+    }
+    return null
+}
+
+const trackedLinkExists: boolean = !!document.querySelector("a[data-track-click]")
+
+if (trackedLinkExists) {
+    document.addEventListener("click", (ev) => {
+        const targetElement = ev.target as HTMLElement
+        const trackedElement = getParent(targetElement, (el: HTMLElement) => el.getAttribute("data-track-click") != null)
+        if (trackedElement) {
+            // Note this will not work on anchor tags without target=_blank, as
+            // they immediately navigate away before the event can be sent.
+            // To handle those we need to wait before navigating.
+            Analytics.logEvent("OWID_SITE_CLICK", {
+                text: trackedElement.innerText,
+                href: trackedElement.getAttribute("href")
+            })
+        }
+    })
+}
