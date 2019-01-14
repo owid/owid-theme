bfca255f2c987eef4e3dcce9f511cfbaa182017e
Jaiden Mispy <jaiden@mispy.me>
2019-01-13 20:26:55 -0800

    identifyadmin => public dir

--- src/BakeWordpress.tsx
+++ src/BakeWordpress.tsx
@@ -193,7 +193,6 @@ export default class WordpressBaker {
     }
 
     async bakeAssets() {
-        shell.exec(`rsync -havz --delete ${WORDPRESS_DIR}/wp-content/themes/owid-theme/identifyadmin.html ${BAKED_DIR}/`)
         shell.exec(`rsync -havz --delete ${WORDPRESS_DIR}/wp-content ${BAKED_DIR}/`)
         shell.exec(`rsync -havz --delete ${WORDPRESS_DIR}/wp-includes ${BAKED_DIR}/`)
         shell.exec(`rsync -havz --delete ${WORDPRESS_DIR}/favicon* ${BAKED_DIR}/`)
